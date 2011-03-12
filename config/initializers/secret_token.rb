f2297c85118693c55369181f9ca552a35e7af79a
Chris Johnson <chris@johnsonch.com>
2011-03-12 11:40:22 -0600

    ok it's rails 3 time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoGallery::Application.config.secret_token = '96a14950395dbbfd09975ec2802a7a3e759c170697a424d89ddf6220a244de9c47ba3070119626617d80a9524a83d4438c422df9c5b2fd473edfde806a5ca058'
